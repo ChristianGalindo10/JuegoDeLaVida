d7a552a24f748d18cc1c5aff676cf22dc28d27cf
ChristianGalindo10 <cygc1@hotmail.com>
2020-12-15 17:23:41 -0500

    cambios modelo

--- src/the_game_of_life/Modelo.java
+++ src/the_game_of_life/Modelo.java
@@ -1,165 +1,104 @@
 /**
  * The game of life
  *
- * @author Marc Oliveras Galvez <admin@oligalma.com> 
+ * @author Marc Oliveras Galvez <admin@oligalma.com>
  * @link http://www.oligalma.com
  * @copyright 2017 Oligalma
  * @license GPL License v3
  */
-
 package the_game_of_life;
 
-public class Modelo
-{
-	// Attributes.
-	private boolean[][] area;
-	private int rowsNumber, colsNumber;
+public class Modelo {
+    // Attributes.
 
-	// Constructor.
-	public Modelo(int rowsNumber, int colsNumber) {
-		this.rowsNumber = rowsNumber;
-		this.colsNumber = colsNumber;
-		this.area = new boolean[rowsNumber][colsNumber];
-	}
+    private boolean[][] area;
+    private int rowsNumber, colsNumber;
 
-	// Method to calculate next generations
-	public void nextGen() {
-		int cell = 0, row, col;
+    // Constructor.
+    public Modelo(int rowsNumber, int colsNumber) {
+        this.rowsNumber = rowsNumber + 2;
+        this.colsNumber = colsNumber + 2;
+        this.area = new boolean[rowsNumber][colsNumber];
+    }
 
-		// Copy area matrix.
-		boolean[][] areaCopy = this.copyArea();
+    // Method to calculate next generations
+    public void nextGen() {
+        int cell = 0, row, col;
 
-		for (row = 0; row < rowsNumber; row++) {
-			for (col = 0; col < colsNumber; col++) {
-				if (row == 0 && col == 0) {
-					if (areaCopy[row][col + 1])
-						cell++;
-					if (areaCopy[row + 1][col])
-						cell++;
-					if (areaCopy[row + 1][col + 1])
-						cell++;
-				} else if (row == 0 && col == colsNumber - 1) {
-					if (areaCopy[row][col - 1])
-						cell++;
-					if (areaCopy[row + 1][col - 1])
-						cell++;
-					if (areaCopy[row + 1][col])
-						cell++;
-				} else if (row == rowsNumber - 1 && col == 0) {
-					if (areaCopy[row - 1][col])
-						cell++;
-					if (areaCopy[row - 1][col + 1])
-						cell++;
-					if (areaCopy[row][col + 1])
-						cell++;
-				} else if (row == rowsNumber - 1 && col == colsNumber - 1) {
-					if (areaCopy[row - 1][col - 1])
-						cell++;
-					if (areaCopy[row - 1][col])
-						cell++;
-					if (areaCopy[row][col - 1])
-						cell++;
-				} else if (row == 0 && col != colsNumber - 1 && col != 0) {
-					if (areaCopy[row][col - 1])
-						cell++;
-					if (areaCopy[row][col + 1])
-						cell++;
-					if (areaCopy[row + 1][col - 1])
-						cell++;
-					if (areaCopy[row + 1][col])
-						cell++;
-					if (areaCopy[row + 1][col + 1])
-						cell++;
-				} else if (row == rowsNumber - 1 && col != colsNumber - 1 && col != 0) {
-					if (areaCopy[row - 1][col - 1])
-						cell++;
-					if (areaCopy[row - 1][col])
-						cell++;
-					if (areaCopy[row - 1][col + 1])
-						cell++;
-					if (areaCopy[row][col - 1])
-						cell++;
-					if (areaCopy[row][col + 1])
-						cell++;
-				} else if (row != rowsNumber - 1 && row != 0 && col == 0) {
-					if (areaCopy[row - 1][col])
-						cell++;
-					if (areaCopy[row - 1][col + 1])
-						cell++;
-					if (areaCopy[row][col + 1])
-						cell++;
-					if (areaCopy[row + 1][col])
-						cell++;
-					if (areaCopy[row + 1][col + 1])
-						cell++;
-				} else if (row != rowsNumber - 1 && row != 0 && col == colsNumber - 1) {
-					if (areaCopy[row - 1][col - 1])
-						cell++;
-					if (areaCopy[row - 1][col])
-						cell++;
-					if (areaCopy[row][col - 1])
-						cell++;
-					if (areaCopy[row + 1][col - 1])
-						cell++;
-					if (areaCopy[row + 1][col])
-						cell++;
-				} else { 
-					if (areaCopy[row - 1][col - 1])
-						cell++;
-					if (areaCopy[row - 1][col])
-						cell++;
-					if (areaCopy[row - 1][col + 1])
-						cell++;
-					if (areaCopy[row][col - 1])
-						cell++;
-					if (areaCopy[row][col + 1])
-						cell++;
-					if (areaCopy[row + 1][col - 1])
-						cell++;
-					if (areaCopy[row + 1][col])
-						cell++;
-					if (areaCopy[row + 1][col + 1])
-						cell++;
-				}
-				if (areaCopy[row][col]) {
-					if (cell == 0 || cell == 1 || cell >= 4)
-						area[row][col] = false;
-				} else {
-					if (cell == 3)
-						area[row][col] = true;
-				}
-				cell = 0;
-			}
-		}
-	}
+        // Copy area matrix.
+        boolean[][] areaCopy = this.copyArea();
 
+        for (row = 0; row < rowsNumber; row++) {
+            for (col = 0; col < colsNumber; col++) {
+                if (row != 0 && row != rowsNumber - 1 && col != 0 && col != colsNumber - 1) {
+                    if (areaCopy[row - 1][col - 1]) {
+                        cell++;
+                    }
+                    if (areaCopy[row - 1][col]) {
+                        cell++;
+                    }
+                    if (areaCopy[row - 1][col + 1]) {
+                        cell++;
+                    }
+                    if (areaCopy[row][col - 1]) {
+                        cell++;
+                    }
+                    if (areaCopy[row][col + 1]) {
+                        cell++;
+                    }
+                    if (areaCopy[row + 1][col - 1]) {
+                        cell++;
+                    }
+                    if (areaCopy[row + 1][col]) {
+                        cell++;
+                    }
+                    if (areaCopy[row + 1][col + 1]) {
+                        cell++;
+                    }
+                    if (areaCopy[row][col]) {
+                        if (cell == 0 || cell == 1 || cell >= 4) {
+                            area[row-1][col-1] = false;
+                        }
+                    } else {
+                        if (cell == 3) {
+                            area[row-1][col-1] = true;
+                        }
+                    }
+                    cell = 0;
+                }
+            }
+        }
+    }
 
-	public boolean[][] getArea()
-	{
-		return area;
-	}
-	
-	public void setArea(boolean[][] area)
-	{
-		this.area = area;
-	}
+    public boolean[][] getArea() {
+        return area;
+    }
 
-	public void clearArea()
-	{
-		for (int i = 0; i < area.length; i++)
-			for (int j = 0; j < area[0].length; j++)
-				area[i][j] = false;	
-	}
-	
-	private boolean[][] copyArea()
-	{
-		boolean[][] areaCopy = new boolean[rowsNumber][colsNumber];
-		
-		for (int row = 0; row < rowsNumber; row++)
-			for (int col = 0; col < colsNumber; col++)
-				areaCopy[row][col] = area[row][col];
-		
-		return areaCopy;
-	}
-	
+    public void setArea(boolean[][] area) {
+        this.area = area;
+    }
+
+    public void clearArea() {
+        for (int i = 0; i < area.length; i++) {
+            for (int j = 0; j < area[0].length; j++) {
+                area[i][j] = false;
+            }
+        }
+    }
+
+    private boolean[][] copyArea() {
+        boolean[][] areaCopy = new boolean[rowsNumber][colsNumber];
+
+        for (int row = 0; row < rowsNumber; row++) {
+            for (int col = 0; col < colsNumber; col++) {
+                if (row == 0 || row == rowsNumber - 1 || col == 0 || col == colsNumber - 1) {
+                    areaCopy[row][col] = false;
+                } else {
+                    areaCopy[row][col] = area[row-1][col-1];
+                }
+            }
+        }
+        return areaCopy;
+    }
+
 }
